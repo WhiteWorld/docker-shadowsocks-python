8bec66f7f6eb5e5f3ce1a25647e2afe9ba603f7c
Liu <ljq258@gmail.com>
2015-05-08 13:21:44 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -8,10 +8,8 @@ if [ $? -ne 0 ];then
 	echo '2607:f8b0:4007:805::100f scholar.l.google.com' >> /etc/hosts
 fi
 
-ifconfig
-
-ping6 -c 4 scholar.google.cn
-
-sleep 2
+# ifconfig
+# ping6 -c 4 scholar.google.cn
+# sleep 2
 
 /usr/local/bin/ssserver -c /etc/shadowsocks/shadowsocks.json
